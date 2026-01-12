0977e5658c112541fde543da48b2539ceebafb84
Uttam <uttamrohit4545@gmail.com>
2026-01-12 21:19:04 +0600

    feat : create post

---
+++ crud.ts
@@ -0,0 +1,29 @@
+
+import { prisma } from './lib/prisma'
+
+async function run () {
+    // const createUser = await prisma.user.create({
+    //     data : {
+    //         name : "Riddi Sharma",
+    //         email : "riddi@gmail.com"
+    //     }
+    // })
+    // console.log("User: ", createUser)
+
+
+    // create post
+    // const createPost = await prisma.post.create({
+    //     data:{
+    //         title : "This is title",
+    //         content : "This is content",
+    //         authorId : 1
+    //     }
+    // })
+    // console.log("Post : ",createPost)
+
+
+
+    
+}
+
+run()
